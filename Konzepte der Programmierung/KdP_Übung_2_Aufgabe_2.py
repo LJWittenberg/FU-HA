4b0ca8965f60a5b59c2f91f26fee33ad5400b9d2
Lennard.J.Wittenberg <163011224+LJWittenberg@users.noreply.github.com>
2024-10-31 13:29:16 +0100

    Add files via upload

--- Konzepte der Programmierung/KdP_Übung_2_Aufgabe_2.py
+++ Konzepte der Programmierung/KdP_Übung_2_Aufgabe_2.py
@@ -32,12 +32,43 @@ def Aufgabe_2e():
     else "10"
     )
 
-def main():
-   Aufgabe_2a()
-   Aufgabe_2b()
-   Aufgabe_2c()
-   Aufgabe_2d() 
-   Aufgabe_2e()
+def Aufgabe_3b():
+    n = int(input("Zahl 1: "))
+    n_2 = int(input("Zahl 2: "))
+    n_3 = int(input("Zahl 3: "))
+    paarweiseverschieden = n != n_3 and n != n_2 and n_2 != n_3
+    print(paarweiseverschieden) 
 
+def Aufgabe_3c():
+    n = int(input("Zahl 1: "))
+    n_2 = int(input("Zahl 2: "))
+    n_3 = int(input("Zahl 3: "))
+    paarweisegleich = n == n_3 and n == n_2 and n_2 == n_3
+    print(paarweisegleich)
+
+def Aufgabe_3d_e():
+    n = int(input("Zahl 1: "))
+    n_2 = int(input("Zahl 2: "))
+    n_3 = int(input("Zahl 3: "))
+    numbers = set()
+    numbers.add(n)
+    numbers.add(n_2)
+    numbers.add(n_3)
+    print(len(numbers))
+    # b
+    print(len(numbers) == 3)
+    # c
+    print(len(numbers) == 1)
+ 
+
+def main():
+   #Aufgabe_2a()
+   #Aufgabe_2b()
+   #Aufgabe_2c()
+   #Aufgabe_2d() 
+   #Aufgabe_2e()
+   #Aufgabe_3b()
+   #Aufgabe_3c()
+    Aufgabe_3d_e()
 
 main()
